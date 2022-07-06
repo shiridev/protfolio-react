f38873020d43189064afc4ec6284d86a40ed4d1a
mohammad hossein <mhsh1993@yahoo.com>
2022-07-06 16:44:47 +0430

    correct about us section

--- src/theme/theme.jsx
+++ src/theme/theme.jsx
@@ -1,19 +1,21 @@
 export const Theme = {
   light: {
     type: "light",
-    white:'white',
+    white: "white",
     green: "#2EC4B6",
     yellow: "#FFD60A",
     carbon: "#495057",
+    grey: "#f8f9fa",
     lightCarbon: "#8D99AE",
     lightGrey: "#F8F9FA",
   },
   dark: {
     type: "dark",
-    white:'#2b2d42',
+    white: "#2b2d42",
     green: "#2EC4B6",
     yellow: "#FFD60A",
     carbon: "#fff",
+    grey: "#f8f9fa",
     lightCarbon: "#fff",
     lightGrey: "#22223b",
   },
